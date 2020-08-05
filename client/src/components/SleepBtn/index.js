ef2d8fc1312627a5bafb2e79bc1e555aaee5850b
lauren garelick <laurengarelick@gmail.com>
2020-08-04 21:09:32 -0700

    ok

--- client/src/components/SleepBtn/index.js
+++ client/src/components/SleepBtn/index.js
@@ -1,54 +1,18 @@
-import React, {useState} from "react";
-import ReactDOM from 'react-dom';
 
+
+import React, {useState} from "react";
+import {sleepWake} from '../../utils/API'
 import "./style.css";
 
 
 function SleepBtn() {
-    const sleepTime = Date.now();
-    function clickMe() {
-        console.log("current time is " + sleepTime)
-    }
+    const [sleepTime, setSleepTime] = useState (Date.now);
+    const [buttonText, setButtonText] = useState("Sleep");
+     //same as creating your state variable where "Next" is the default value for buttonText and setButtonText is the setter function for your state variable instead of setState
 
-    return (
-        <div>
-            <button type="button" className="btn btn-success" onClick={clickMe}>Sleep</button>
-        </div>
-    )
+return  <button onClick={() => setButtonText("Wake Up")}>{buttonText}</button>
+
 }
 
 export default SleepBtn;
-
-// import React, {useState} from "react";
-// import ReactDOM from 'react-dom';
-// import "./style.css";
-
-
-// function SleepBtn() {
-//     const [sleepTime, setSleepTime] = useState (Date.now);
-//     const [background, setBackground] = useState (backgroundImge[client\src\index.css])
-   
-//     useEffect (() => {
-//     function changeSleepStatus (asleep) {
-//         setSleepTime(sleepTime(Date.now));
-        
-//     }
-//    }
-//    );
-//     const sleepTime = Date.now();
-//     function clickMe() {
-//         console.log("current time is " + sleepTime)
-//     }
-
-//     return (
-//         <div>
-//             <button onClick={() => setSleep(sleep)}>
-//     Click me
-//   </button>
-//             <button type="button" className="btn btn-success" onClick={clickMe}>Sleep</button>
-//         </div>
-//     )
-// }
-
-// export default SleepBtn;
 
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,3 +3,4 @@ import axios from 'axios';
 export const signup = user => axios.post("/api/auth/signup", user);
 export const login = user => axios.post("/api/auth/login", user);
 export const logout = () => axios.get("/api/auth/logout");
+export const sleepWake = (data,id) => axios.put("/api/sleep/newentry/"+id, data);
