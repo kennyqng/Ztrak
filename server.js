be0f281d4a11e3c94de21c0f84e41a928e02cba3
Rogelio Salgado <rogelis@uci.edu>
2020-08-06 13:43:04 -0700

    added the atlas connections to server and env

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require("path");
+const cors = require('cors'); //cors for atlas
+const mongoose = require('mongoose'); //mongoose for atlas
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const session = require("express-session");
@@ -7,8 +11,19 @@ const passport = require("./config/passport")
 const routes = require("./routes");
 
 // Define middleware here
+app.use(cors()); //mongodbAtlas
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Atlas connection
+const uri = process.env.ATLAS_URI || "mongodb://localhost/ztrakdev.db";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
